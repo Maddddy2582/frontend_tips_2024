9a044af5241fe9337517d835012d1d934fb09fda
Madav mahesh <madav.mahesh@solitontech.local>
2024-02-01 13:50:20 +0530

    fix: changed based on PR review

--- src/javascript/node.js
+++ src/javascript/node.js
@@ -1,10 +1,12 @@
-let cityData;
+'use strict'
+let cityData
 let city;
 (async () => {
-  const data = await fetch('../../docs/assets/Images/HTML & CSS/files/data.json')
+  const data = await fetch(
+    '../../docs/assets/Images/HTML & CSS/files/data.json'
+  )
   cityData = await data.json()
   topContainer()
-  midcontainer()
 })()
 
 /**
@@ -12,161 +14,228 @@ let city;
  * @param imgid
  * @param city
  */
-function topContainer(){
+
+// Function when selecting a city from dropdown
+/**
+ *
+ */
+function topContainer () {
   const cityInput = document.querySelector('.city-input')
-  cityInput.addEventListener('change',(e)=>{
-    city=e.target.value
-    console.log(city)
-    if(Object.keys(cityData).includes(city)){
+  cityInput.addEventListener('change', (e) => {
+    city = e.target.value
+    if (Object.keys(cityData).includes(city)) {
       updatedTime()
-    }else{
+    } else {
       resetTime()
     }
   })
 }
 
-function changeTempValues()
-{
-  document.getElementById('change-temp-now').innerText = `${(cityData[city].temperature)}`
-  document.getElementById('change-temp1').innerText = `${(cityData[city].nextFiveHrs[0])}`
-  document.getElementById('change-temp2').innerText = `${(cityData[city].nextFiveHrs[1])}`
-  document.getElementById('change-temp3').innerText = `${(cityData[city].nextFiveHrs[2])}`
-  document.getElementById('change-temp4').innerText = `${(cityData[city].nextFiveHrs[3])}`
+// Fuction to change next 5 hrs temperature value
+/**
+ *
+ */
+function changeTempValues () {
+  document.getElementById(
+    'change-temp-now'
+  ).innerText = `${cityData[city].temperature}`
+  document.getElementById(
+    'change-temp1'
+  ).innerText = `${cityData[city].nextFiveHrs[0]}`
+  document.getElementById(
+    'change-temp2'
+  ).innerText = `${cityData[city].nextFiveHrs[1]}`
+  document.getElementById(
+    'change-temp3'
+  ).innerText = `${cityData[city].nextFiveHrs[2]}`
+  document.getElementById(
+    'change-temp4'
+  ).innerText = `${cityData[city].nextFiveHrs[3]}`
 }
 
-function changeState(state1)
-{
-  if (state1 === 'PM') {
-    document.getElementById('state-img').src = '../../docs/assets/Images/HTML & CSS/General Images & Icons/pmState.svg'
+// Function to change AM and PM states
+/**
+ *
+ * @param   state
+ */
+function changeState (state) {
+  if (state === 'PM') {
+    document.getElementById('state-img').src =
+      '../../docs/assets/Images/HTML & CSS/General Images & Icons/pmState.svg'
   } else {
-    document.getElementById('state-img').src = '../../docs/assets/Images/HTML & CSS/General Images & Icons/amState.svg'
+    document.getElementById('state-img').src =
+      '../../docs/assets/Images/HTML & CSS/General Images & Icons/amState.svg'
   }
 }
 
-function changeMainImage(city)
-{
-  document.getElementById('city-img').src = '../../docs/assets/Images/HTML & CSS/Icons for cities/' + city + '.svg'
+// Function to change main city images
+/**
+ *
+ * @param city
+ */
+function changeMainImage (city) {
+  document.getElementById('city-img').src =
+    '../../docs/assets/Images/HTML & CSS/Icons for cities/' + city + '.svg'
 }
 
-function changeFourValues(city)
-{
-  document.getElementById('tempid').innerText = `${(cityData[city].temperature)}`
+// Function to change all weather values
+/**
+ *
+ * @param city
+ */
+function changeFourValues (city) {
   const [celciusVal] = cityData[city].temperature.split('\u00B0')
-  document.getElementById('farenid').innerText = (celciusVal * 1.8 + 32).toFixed(1) + ' F'
-  document.getElementById('humidityid').innerText = `${(cityData[city].humidity)}`
-  document.getElementById('precipitationid').innerText = `${(cityData[city].precipitation)}`
+  document.getElementById('tempid').innerText = `${cityData[city].temperature}`
+  document.getElementById('farenid').innerText =
+    (celciusVal * 1.8 + 32).toFixed(1) + ' F'
+  document.getElementById(
+    'humidityid'
+  ).innerText = `${cityData[city].humidity}`
+  document.getElementById(
+    'precipitationid'
+  ).innerText = `${cityData[city].precipitation}`
 }
 
-function hours(hr,state1)
-{
+// Function to change next 5 hrs time
+/**
+ *
+ * @param hr
+ * @param   state
+ */
+function hours (hr, state) {
   const changeTime = document.querySelectorAll('.change-time')
-  const timeForcast = Array.from(changeTime).map((element) => element.textContent)
+  const timeForcast = Array.from(changeTime).map(
+    (element) => element.textContent
+  )
   for (let i = 0, currentTime = hr; i < timeForcast.length; i++) {
-    if (state1 === 'AM') {
-      if (currentTime < 11) {
-        currentTime++
-        timeForcast[i] = currentTime + 'AM'
-        changeTime[i].textContent = timeForcast[i]
-      } else if (currentTime === '11') {
-        currentTime++
-        timeForcast[i] = currentTime + 'PM'
-        state = 'PM'
-        changeTime[i].textContent = timeForcast[i]
-      } else if (currentTime === '12') {
-        timeForcast[i] = 1 + 'AM'
-        currentTime = 1
-        state1 = 'AM'
-        changeTime[i].textContent = timeForcast[i]
-      }
+    if (state === 'AM') {
+      changeStateAm(currentTime, timeForcast, changeTime, state, i)
     } else {
-      if (currentTime < 11) {
-        currentTime++
-        timeForcast[i] = currentTime + 'PM'
-        changeTime[i].textContent = timeForcast[i]
-      } else if (currentTime === '11') {
-        currentTime++
-        timeForcast[i] = currentTime + 'AM'
-        state1 = 'AM'
-        changeTime[i].textContent = timeForcast[i]
-      } else if (currentTime === '12') {
-        timeForcast[i] = 1 + 'PM'
-        currentTime = 1
-        state1 = 'PM'
-        changeTime[i].textContent = timeForcast[i]
-      }
+      changeStatePm(currentTime, timeForcast, changeTime, state, i)
     }
   }
 }
 
-//----------------------------------------------------------------------------------------------------------------------------
-  function updatedTime(){
-    const absoluteTime=new Date().toLocaleString("en-US",{timeZone:cityData[city].timeZone})
+// Function to change state to AM based on time
+/**
+ *
+ * @param currentTime
+ * @param timeForcast
+ * @param changeTime
+ * @param   state
+ * @param i
+ */
+function changeStateAm (currentTime, timeForcast, changeTime, state, i) {
+  if (currentTime < 11) {
+    currentTime++
+    timeForcast[i] = currentTime + 'AM'
+    changeTime[i].textContent = timeForcast[i]
+  } else if (currentTime === '11') {
+    currentTime++
+    timeForcast[i] = currentTime + 'PM'
+    state = 'PM'
+    changeTime[i].textContent = timeForcast[i]
+  } else if (currentTime === '12') {
+    timeForcast[i] = 1 + 'AM'
+    currentTime = 1
+    state = 'AM'
+    changeTime[i].textContent = timeForcast[i]
+  }
+}
 
+// Function to change state to PM based on time
+/**
+ *
+ * @param currentTime
+ * @param timeForcast
+ * @param changeTime
+ * @param   state
+ * @param i
+ */
+function changeStatePm (currentTime, timeForcast, changeTime, state, i) {
+  if (currentTime < 11) {
+    currentTime++
+    timeForcast[i] = currentTime + 'PM'
+    changeTime[i].textContent = timeForcast[i]
+  } else if (currentTime === '11') {
+    currentTime++
+    timeForcast[i] = currentTime + 'AM'
+    state = 'AM'
+    changeTime[i].textContent = timeForcast[i]
+  } else if (currentTime === '12') {
+    timeForcast[i] = 1 + 'PM'
+    currentTime = 1
+    state = 'PM'
+    changeTime[i].textContent = timeForcast[i]
+  }
+}
 
-  
+// Function to assign proper weather icon
+/**
+ *
+ * @param changingValue
+ * @param j
+ */
+function changeimg (changingValue, j) {
+  if (changingValue > 23 && changingValue < 29) {
+    document.querySelectorAll('.cloud-img')[j].src =
+      '../../docs/assets/Images/HTML & CSS/Weather Icons/cloudyIcon.svg'
+  } else if (changingValue <= 18) {
+    document.querySelectorAll('.cloud-img')[j].src =
+      '../../docs/assets/Images/HTML & CSS/Weather Icons/rainyIconBlack.svg'
+  } else if (changingValue > 18 && changingValue <= 22) {
+    document.querySelectorAll('.cloud-img')[j].src =
+      '../../docs/assets/Images/HTML & CSS/Weather Icons/windyIcon.svg'
+  } else {
+    document.querySelectorAll('.cloud-img')[j].src =
+      '../../docs/assets/Images/HTML & CSS/Weather Icons/sunnyIcon.svg'
+  }
+}
+
+// Function when a proper city is selected
+/**
+ *
+ */
+function updatedTime () {
+  const absoluteTime = new Date().toLocaleString('en-US', {
+    timeZone: cityData[city].timeZone
+  })
   const [date, time] = absoluteTime.split(', ')
   document.getElementById('datejs').innerText = date.replaceAll('/', '-')
-  let [currenttime, state] = time.split(' ')
+  const [currenttime, state] = time.split(' ')
   const [hr, min, sec] = currenttime.split(':')
   document.getElementById('time').innerText = hr + ':' + min
   document.getElementById('sec').innerText = ':' + sec
-  document.querySelector('.hourly-info').style.visibility='visible'
- 
-  
-  
+  document.querySelector('.hourly-info').style.visibility = 'visible'
   changeTempValues()
   changeState(state)
   changeMainImage(city)
   changeFourValues(city)
-  hours(hr,state)
-
-
-
+  hours(hr, state)
   const changeTempClass = document.querySelectorAll('.tempValue')
-  const cloudTemp = Array.from(changeTempClass).map((element) => element.textContent)
-  for(let j=0;j<5;j++)
-  {
-    let[cloudTempData]=cloudTemp[j].split('\u00B0')
-    cloudTempData=Number(cloudTempData)
-    changeimg(cloudTempData,j)
+  const cloudTemp = Array.from(changeTempClass).map(
+    (element) => element.textContent
+  )
+  for (let j = 0; j < 5; j++) {
+    let [cloudTempData] = cloudTemp[j].split('\u00B0')
+    cloudTempData = Number(cloudTempData)
+    changeimg(cloudTempData, j)
   }
-  function changeimg(changingValue,j)
-  {
-    if(changingValue>23 && changingValue<29)
-    {
-      document.querySelectorAll('.cloud-img')[j].src = '../../docs/assets/Images/HTML & CSS/Weather Icons/cloudyIcon.svg'
-    }
-    else if(changingValue<=18)
-    {
-      document.querySelectorAll('.cloud-img')[j].src = '../../docs/assets/Images/HTML & CSS/Weather Icons/rainyIconBlack.svg'
-    }
-    else if(changingValue>18 && changingValue<=22)
-    {
-      document.querySelectorAll('.cloud-img')[j].src = '../../docs/assets/Images/HTML & CSS/Weather Icons/windyIcon.svg'
-    }
-    else
-    {
-      document.querySelectorAll('.cloud-img')[j].src = '../../docs/assets/Images/HTML & CSS/Weather Icons/sunnyIcon.svg'
-    }
-  }
-  setTimeout(updatedTime,1000)
-}
-function resetTime()
-{
-  alert("Invalid City")
-  document.getElementById('tempid').innerText = "---"
-  document.getElementById('farenid').innerText = "---"
-  document.getElementById('humidityid').innerText = "---"
-  document.getElementById('precipitationid').innerText = "---"
-  document.getElementById('time').innerText = "--:--"
-  document.getElementById('sec').innerText =" "
-  document.getElementById('datejs').innerText ="__-__-__"
-  document.querySelector('.hourly-info').style.visibility='hidden'
+  setTimeout(updatedTime, 1000)
 }
 
-
-
-
-
-
-
+// Function When proper city name is not given
+/**
+ *
+ */
+function resetTime () {
+  alert('Invalid City')
+  document.getElementById('tempid').innerText = '---'
+  document.getElementById('farenid').innerText = '---'
+  document.getElementById('humidityid').innerText = '---'
+  document.getElementById('precipitationid').innerText = '---'
+  document.getElementById('time').innerText = '--:--'
+  document.getElementById('sec').innerText = ' '
+  document.getElementById('datejs').innerText = '__-__-__'
+  document.querySelector('.hourly-info').style.visibility = 'hidden'
+}
