4667c63ade4996aa25236b4b81c8350b318a2412
madav mahesh <madav.mahesh@solitontech.com>
2024-02-13 19:00:10 +0530

    chore: fix scroll issue

--- src/javascript/midcontainer/midcontainer.js
+++ src/javascript/midcontainer/midcontainer.js
@@ -1,9 +1,16 @@
-let cardNum;
+// let cardNum;
 const mainCityContainer = document.querySelector(".main-city-container");
-
+let counterValue = document.querySelector(".cities-counter");
+counterValue.value = 3;
 export function midContainer(cityData) {
-  hidebutton();
+  // hidebutton();
+  // function defaultSortType() {
+  //   cloningCards("sunny", sunnycities, cityData);
+  // }
+  // defaultSortType();
 
+  hideLeftButton();
+  hideRightButton();
   const cityValue = document.querySelector(".city-input");
   cityValue.addEventListener("change", (g) => {
     city = g.target.value;
@@ -41,85 +48,76 @@ export function midContainer(cityData) {
   //----------------------SUNNY CITIES---------------------------------------------
   const sunnyIconClass = document.getElementById("sunny-icon-class");
   sunnyIconClass.addEventListener("click", function () {
-    hidebutton();
+    counterValue.value = 3;
+    hideLeftButton();
+    hideRightButton();
+    // hidebutton();
     sunnyIconClass.classList.add("border-bottom");
     snowyIconClass.classList.remove("border-bottom");
     rainyIconClass.classList.remove("border-bottom");
     cloningCards("sunny", sunnycities, cityData);
-    document.querySelector(".city-card-container").style.animation =
-      "wipe-enter 1s";
-
     document.querySelector(".cities-counter").disabled = true;
   });
 
   //---------------------------SNOWY CITIES--------------------------------
   const snowyIconClass = document.getElementById("snowy-icon-class");
   snowyIconClass.addEventListener("click", function () {
-    hidebutton();
+    counterValue.value = 2;
+    hideLeftButton();
+    hideRightButton();
     sunnyIconClass.classList.remove("border-bottom");
     snowyIconClass.classList.add("border-bottom");
     rainyIconClass.classList.remove("border-bottom");
     cloningCards("snowflake", snowycities, cityData);
-    document.querySelector(".city-card-container").style.animation =
-      "wipe-enter 1s";
     document.querySelector(".cities-counter").disabled = true;
   });
 
   //--------------RAINY CITIES-------------------------------------------------
   const rainyIconClass = document.getElementById("rainy-icon-class");
   rainyIconClass.addEventListener("click", function () {
-    showbutton();
+    counterValue.value = 3;
     sunnyIconClass.classList.remove("border-bottom");
     snowyIconClass.classList.remove("border-bottom");
     rainyIconClass.classList.add("border-bottom");
-    let counterValue = document.querySelector(".cities-counter").value;
     document.querySelector(".cities-counter").disabled = false;
-
-    cloningCards("rainy", rainycities.slice(0, counterValue), cityData);
+    cloningCards("rainy", rainycities.slice(0, 3), cityData);
+    dynamicHidingButtons(mainCityContainer);
 
     let numberOfCities = document.querySelector(".cities-counter");
     numberOfCities.addEventListener("change", (f) => {
-      showbutton();
-      document.querySelector(".city-card-container").style.animation = "none";
-      cardNum = f.target.value;
-      cloningCards("rainy", rainycities.slice(0, cardNum), cityData);
+      let cardNum = f.target.value;
+      cloningCards(
+        "rainy",
+        rainycities.slice(0, cardNum),
+        cityData,
+        "animation:none;"
+      );
+      if (cardNum > 4) {
+        mainCityContainer.scrollLeft += 4000;
+      }
+      dynamicHidingButtons(mainCityContainer);
     });
   });
   document.querySelector("#leftbutton").addEventListener("click", function () {
-    document.querySelector(".main-city-container").scrollLeft -= 260;
+    document.querySelector(".main-city-container").scrollLeft -= 268;
+    dynamicHidingButtons(mainCityContainer);
   });
   document.querySelector("#rightbutton").addEventListener("click", function () {
-    document.querySelector(".main-city-container").scrollLeft += 260;
+    document.querySelector(".main-city-container").scrollLeft += 268;
+    dynamicHidingButtons(mainCityContainer);
   });
 }
 //---------------------------------------------------------
-function hidebutton() {
-  document.querySelector("#leftbutton").style.visibility = "hidden";
-  document.querySelector("#rightbutton").style.visibility = "hidden";
-}
+function cloningCards(sunny, selectedCityArray, cityData, anime) {
+  // console.log(cityCardContainer);
 
-function showbutton() {
-  let counterValue = document.querySelector(".cities-counter");
-  if (counterValue.value > 5) {
-    document.querySelector("#leftbutton").style.visibility = "visible";
-    document.querySelector("#rightbutton").style.visibility = "visible";
-    document.querySelector(".main-city-container").scrollLeft += 310;
-  } else {
-    document.querySelector("#leftbutton").style.visibility = "hidden";
-    document.querySelector("#rightbutton").style.visibility = "hidden";
-  }
-}
-function setTime(liveTime, nodeList, j) {
-  let [date, time] = liveTime.split(", ");
-  nodeList[j].querySelector(".card-city-time").textContent = `${time}`;
-  return date;
-}
-
-function cloningCards(sunny, selectedCityArray, cityData) {
   mainCityContainer.innerHTML = "";
-  selectedCityArray.forEach(function (city) {
+  selectedCityArray.forEach(function (city, index) {
+    if (index === selectedCityArray.length - 1)
+      anime = "animation:wipe-enter 1s;";
+    let [updateDate, updateTime] = updateDateTime(cityData, city);
     const html = `
-    <div class="city-card-container" style = "background-image: url('../../../docs/assets/Images/HTML & CSS/Icons for cities/${city}.svg');">
+    <div class="city-card-container" style = "background-image: url('../../../docs/assets/Images/HTML & CSS/Icons for cities/${city}.svg');${anime}">
       <div class="card-city-name">${city}</div>
       <div class="card-city-icon">
         <img
@@ -127,8 +125,8 @@ function cloningCards(sunny, selectedCityArray, cityData) {
           alt="sunnyicon"
         /><span id="card-city-temp">${cityData[city].temperature}</span>
       </div>
-      <div class="card-city-time">10:00AM</div>
-      <div class="card-city-date">2-MAR-2020</div>
+      <div class="card-city-time">${updateTime}</div>
+      <div class="card-city-date">${updateDate}</div>
       <div class="card-city-humidity">
         <img
           src="../../docs/assets/Images/HTML & CSS/Weather Icons/humidityIcon.svg"
@@ -144,4 +142,73 @@ function cloningCards(sunny, selectedCityArray, cityData) {
     </div>`;
     mainCityContainer.insertAdjacentHTML("beforeend", html);
   });
+}
+function updateDateTime(cityData, city) {
+  const absoluteTime = new Date().toLocaleString("en-US", {
+    timeZone: cityData[city].timeZone,
+  });
+  let [date, time] = absoluteTime.split(", ");
+  const [currenttime, state] = time.split(" ");
+  const [hours, min, sec] = currenttime.split(":");
+  const [month, day, year] = date.split("/");
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Aug",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  date = day + "-" + months[month - 1] + "-" + year;
+  const hour = hours + ":" + min + " " + state;
+  return [date, hour];
+}
+function dynamicHidingButtons(mainCityContainer) {
+  if (mainCityContainer.scrollWidth > mainCityContainer.clientWidth) {
+    if (mainCityContainer.scrollLeft > 0) {
+      showLeftButton();
+    }
+    if (mainCityContainer.scrollLeft === 0) {
+      showRightButton();
+      hideLeftButton();
+    }
+    if (
+      mainCityContainer.scrollWidth - mainCityContainer.scrollLeft >
+      mainCityContainer.clientWidth
+    ) {
+      showRightButton();
+    }
+    if (
+      mainCityContainer.scrollWidth - mainCityContainer.scrollLeft <
+      mainCityContainer.clientWidth
+    ) {
+      hideRightButton();
+    }
+  } else {
+    hideLeftButton();
+    hideRightButton();
+  }
+}
+
+function showLeftButton() {
+  document.querySelector("#leftbutton").style.visibility = "visible";
+}
+
+function showRightButton() {
+  document.querySelector("#rightbutton").style.visibility = "visible";
+}
+
+function hideLeftButton() {
+  document.querySelector("#leftbutton").style.visibility = "hidden";
+}
+
+function hideRightButton() {
+  document.querySelector("#rightbutton").style.visibility = "hidden";
 }
