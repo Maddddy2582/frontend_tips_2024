2f1925689121be07f869a62ba1af4e759eb9f804
madav mahesh <madav.mahesh@solitontech.com>
2024-03-02 09:24:26 +0530

    feat: final commit

--- src/New folder/script.js
+++ src/New folder/script.js
@@ -1,3 +1,4 @@
+// Selecting all buttons
 const signInBtn = document.querySelector(".btn-signin");
 const addBoardBtn = document.querySelector(".add-board-btn");
 const btnBoardBack = document.querySelector(".btn-board-back");
@@ -19,7 +20,11 @@ const btnBackRemoveMember = document.querySelector(".btn-back-remove-member");
 const btnTaskBackMember = document.querySelector(".btn-task-back-member");
 const btnSummaryBackMember = document.querySelector(".btn-summary-back-member");
 const btnAddTask = document.querySelector(".btn-add-task");
+const btnModifyTask = document.querySelector(".btn-modify-task");
+const btnModifyTaskConfirm = document.querySelector(".btn-modify-task-confirm");
+const btnModifyBack = document.querySelector(".btn-modify-back");
 
+// Selecting all the input values
 const inputNameSignin = document.querySelector(".input-name-signin");
 const inputPasswordSignin = document.querySelector(".input-password-signin");
 const selectUserType = document.querySelector(".select-usertype");
@@ -30,7 +35,16 @@ const selectMember = document.querySelector(".select-member");
 const selectTasktype = document.querySelector(".select-tasktype");
 const listOfMembers = document.querySelectorAll(".list-of-members");
 const listOfProjects = document.querySelector(".list-of-projects");
+const taskDescp = document.querySelector(".task-descp");
+const inputModifyName = document.querySelector(".input-modify-name");
+const inputModifyDuedate = document.querySelector(".input-modify-duedate");
+const modifyListOfMembers = document.querySelector(".modify-list-of-members");
+const modifyTasktype = document.querySelector(".modify-tasktype");
+const modifyTaskDescp = document.querySelector(".modify-task-descp");
+const pendingTasksLabel = document.querySelector(".pending-tasks-label");
+const membersNameLabel = document.querySelector(".members-name-label");
 
+// Selecting all the panels to display
 const adminHomepage = document.querySelector(".admin-homepage");
 const logInPage = document.querySelector(".log-in-page");
 const memberHomepage = document.querySelector(".member-homepage");
@@ -45,15 +59,23 @@ const addTaskPageMember = document.querySelector(".add-task-page-member");
 const projectSummaryPageMember = document.querySelector(
   ".project-summary-page-member"
 );
+const modifyTaskPage = document.querySelector(".modify-task-page");
 
+// Selecting all the labels
 const adminWelcomeMsg = document.querySelector(".admin-welcome");
 const memberWelcomeMsg = document.querySelector(".member-welcome");
 const leaderNameLabel = document.querySelector(".leader-name-label");
 
+// Selecting all the panels that need to be hidden
 const allSections = document.querySelectorAll(".hidden");
 
+// Required Global variable
 let projectTracker = 1;
+let taskTracker = 1;
+let dragObj;
+usersList = [];
 
+// Class for user
 class User {
   userName = "";
   password = "";
@@ -67,9 +89,12 @@ class User {
   }
 }
 
+// Class for the whole UI
 class Webpage {
   currentuser = undefined;
   currentproject = undefined;
+  currenttask = undefined;
+  allTasks = [];
 
   signin(username, userpassword) {
     if (!usersList.length) {
@@ -95,24 +120,26 @@ class Webpage {
     if (!credentials) alert("Wrong credentials");
   }
 
+  // Method to create a board
   createBoard() {
     let user = this.currentuser;
     user.projects.push(new Project(inputProjectName.value, user.userName));
     let HTML = `<div class="board-container ${inputProjectName.value} hide-board"  id="${projectTracker}">
     <h4 class="board-label">${inputProjectName.value}</h4>
     <div class="main-board">
-      <div class="Pending">Pending</div>
-      <div class="In-Progress">In Progress</div>
-      <div class="Completed">Completed</div>
+      <div class="Pending dropBox">Pending</div>
+      <div class="In-Progress dropBox">In Progress</div>
+      <div class="Completed dropBox">Completed</div>
     </div>
   </div>`;
     adminHomePageBoard.insertAdjacentHTML("beforeend", HTML);
     this.showSection(adminHomepage);
-
     let projectName = `<option>${inputProjectName.value}</option>`;
     listOfProjects.insertAdjacentHTML("beforeend", projectName);
+    projectTracker++;
   }
 
+  // Method to show the selected project
   showSelectedBoard(selectedBoard) {
     let currentBoard = document.getElementsByClassName(`${selectedBoard}`);
     this.currentproject = currentBoard[0];
@@ -123,14 +150,46 @@ class Webpage {
     currentBoard[0].classList.remove("hide-board");
   }
 
-  createTask(name, deadline, member, priority) {
-    let userProjects = this.currentuser.projects[0];
-    userProjects.tasks.push(
-      new Task(name, deadline, member, priority, "this is a task")
-    );
+  // Metod to create a task
+  createTask(name, deadline, member, priority, description) {
+    this.allTasks.push(new Task(name, deadline, member, priority, description));
     let prioritySection = this.currentproject.querySelector(`.${priority}`);
-    let HTML = `<div class="card"><p class="task-message">${name}<p></div>`;
+    let HTML = `<div class="card"  draggable="true" id="task-${taskTracker}">${name}</div>`;
+    taskTracker++;
     prioritySection.insertAdjacentHTML("beforeend", HTML);
+    const dropbox = document.querySelectorAll(".dropBox");
+    dropbox.forEach((element) => {
+      element.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+      element.addEventListener("drop", () => {
+        element.appendChild(dragObj);
+      });
+    });
+  }
+
+  // Method to Modify Task
+  modifyTask() {
+    popup.style.display = "none";
+    inputModifyName.value = `${this.currenttask.name}`;
+    inputModifyDuedate.value = `${this.currenttask.deadline}`;
+    modifyTasktype.value = `${this.currenttask.priority}`;
+    modifyTaskDescp.value = `${this.currenttask.description}`;
+  }
+
+  // Method to confirm the modifications
+  confirmModify() {
+    this.showSection(adminHomepage);
+    let mainBoard = document.querySelector(".main-board");
+    this.currenttask.name = inputModifyName.value;
+    this.currenttask.deadline = inputModifyDuedate.value;
+    this.currenttask.member = modifyListOfMembers.value;
+    this.currenttask.priority = modifyTasktype.value;
+    this.currenttask.description = modifyTaskDescp.value;
+    let currentTaskLabel = mainBoard.querySelector(
+      `#task-${this.currenttask.taskid}`
+    );
+    currentTaskLabel.textContent = `${inputModifyName.value}`;
   }
 
   // Method to display the required panel
@@ -143,7 +202,7 @@ class Webpage {
 }
 
 class Project {
-  static productIdgenerator = 1;
+  static projectIdgenerator = 1;
   name = "";
   creatorName = "";
   tasks = [];
@@ -151,8 +210,8 @@ class Project {
   constructor(name, creatorName) {
     this.name = name;
     this.creatorName = creatorName;
-    this.productId = Project.productIdgenerator;
-    Project.productIdgenerator++;
+    this.projectId = Project.projectIdgenerator;
+    Project.projectIdgenerator++;
   }
 }
 
@@ -163,15 +222,17 @@ class Task {
   member = "";
   priority = "";
   description = "";
-
   constructor(name, deadline, member, priority, description) {
+    this.taskid = Task.taskIdgenerator;
     this.name = name;
     this.deadline = deadline;
     this.member = member;
     this.priority = priority;
     this.description = description;
+    Task.taskIdgenerator++;
   }
 }
+
 const interface = new Webpage();
 
 const addUser = function (userName, password, isAdmin) {
@@ -183,8 +244,6 @@ const addUser = function (userName, password, isAdmin) {
     }
   });
 };
-
-usersList = [];
 
 signInBtn.addEventListener("click", function () {
   if (!inputNameSignin.value || !inputPasswordSignin.value) {
@@ -225,21 +284,19 @@ btnSummaryBack.addEventListener("click", function () {
 });
 
 adminSummaryBtn.addEventListener("click", function () {
-  leaderNameLabel.textContent = `Admin Name: ${interface.currentuser.userName} `;
+  leaderNameLabel.textContent = `Admin Name: ${interface.currentuser.userName}`;
+  pendingTasksLabel.textContent = `Number of tasks remining: ${interface.allTasks.length}`;
+  interface.allTasks.forEach((element) => {
+    membersNameLabel.innerHTML = "";
+    let HTML = `<p>${element.member}</p>`;
+    membersNameLabel.insertAdjacentHTML("beforeend", HTML);
+  });
   interface.showSection(projectSummaryPage);
 });
 
 adminLogout.addEventListener("click", function () {
   interface.showSection(logInPage);
 });
-
-// addMemberBtn.addEventListener("click", function () {
-//   interface.showSection(addNewMember);
-// });
-
-// btnBackAddMember.addEventListener("click", function () {
-//   interface.showSection(adminHomepage);
-// });
 
 memberAddTaskBtn.addEventListener("click", function () {
   interface.showSection(addTaskPageMember);
@@ -252,14 +309,6 @@ memberProjectSummaryBtn.addEventListener("click", function () {
 memberLogout.addEventListener("click", function () {
   interface.showSection(logInPage);
 });
-
-// removeMemberBtn.addEventListener("click", function () {
-//   interface.showSection(removeMember);
-// });
-
-// btnBackRemoveMember.addEventListener("click", function () {
-//   interface.showSection(adminHomepage);
-// });
 
 btnTaskBackMember.addEventListener("click", function () {
   interface.showSection(memberHomepage);
@@ -278,59 +327,66 @@ btnAddTask.addEventListener("click", function () {
     inputTaskName.value,
     inputDuedate.value,
     selectMember.value,
-    selectTasktype.value
+    selectTasktype.value,
+    taskDescp.value
   );
   inputTaskName.value = "";
   inputDuedate.value = "";
   selectMember.value = "";
   selectTasktype.value = "";
+  taskDescp.value = "";
   interface.showSection(adminHomepage);
 });
 
 listOfProjects.addEventListener("change", function () {
   interface.showSelectedBoard(listOfProjects.value);
 });
+
+adminHomepage.addEventListener("dragstart", function (e) {
+  dragObj = e.target;
+});
+
+adminHomepage.addEventListener("click", function (e) {
+  interface.allTasks.forEach((element) => {
+    if (`task-${element.taskid}` === e.target.getAttribute("id")) {
+      popup.style.display = "flex";
+      interface.currenttask = element;
+      const taskName = document.querySelector(".task-name");
+      const taskDuedate = document.querySelector(".task-duedate");
+      const taskMember = document.querySelector(".task-member");
+      const taskPriority = document.querySelector(".task-priority");
+      const taskDescription = document.querySelector(".task-description");
+      taskName.textContent = `Task Name: ${element.name}`;
+      taskDuedate.textContent = `Due Date: ${element.deadline}`;
+      taskMember.textContent = `Member Assigned: ${element.member}`;
+      taskPriority.textContent = `Priority: ${element.priority}`;
+      taskDescription.textContent = `Description: ${element.description}`;
+    }
+    window.addEventListener("click", function (event) {
+      if (event.target === popup) {
+        popup.style.display = "none";
+      }
+    });
+  });
+});
+
+btnModifyTask.addEventListener("click", function () {
+  interface.showSection(modifyTaskPage);
+  interface.modifyTask();
+});
+
+btnModifyTaskConfirm.addEventListener("click", function () {
+  interface.confirmModify();
+});
+
+btnModifyBack.addEventListener("click", function () {
+  interface.showSection(adminHomepage);
+});
+
+// Show the login page
 interface.showSection(logInPage);
+
 // Hard coded Users
-addUser("2", "2", true);
+addUser("John", "Smith", true);
 addUser("Alice", "Parker", false);
-
-// createNewAccountBtn.addEventListener("click", function () {});
-
-// const createNewUser = function () {
-//   if (
-//     !inputNameCreate.value ||
-//     !inputPasswordCreate.value ||
-//     !selectUserType.value
-//   ) {
-//     alert("Text field can't be blank");
-//     return;
-//   }
-//   if (selectUserType.value === "Admin") {
-//     settype = true;
-//   } else {
-//     settype = false;
-//   }
-//   addUser(inputNameCreate.value, inputPasswordCreate.value, settype);
-//   inputNameCreate.value = "";
-//   inputPasswordCreate.value = "";
-// };
-
-// createAccountBtn.addEventListener("click", function () {
-//   if (usersList.length == 0) {
-//     createNewUser();
-//   } else {
-//     let userexist = false;
-//     usersList.forEach((element) => {
-//       if (element.userName == inputNameCreate.value) {
-//         userexist = true;
-//         alert("Username taken :(");
-//         inputNameCreate.value = "";
-//         inputPasswordCreate.value = "";
-//       }
-//     });
-//     if (!userexist) {
-//       createNewUser();
-//     }
-//   }
-// });
+addUser("tony", "stark", false);
